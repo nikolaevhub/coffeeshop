51f79f8a2de6b4b98258e288a2474f6a0b246f21
Артем <nikolaev7@inbox.ru>
2021-03-11 20:20:44 +0300

    Updates

---
+++ precache-manifest.2b8ddbfa2ca9b37d5f2b5fcd889d2e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4d496467e1a48f27a7da7d071eca71",
+    "url": "/coffeeshop/index.html"
+  },
+  {
+    "revision": "3cb666d9747f286252ac",
+    "url": "/coffeeshop/static/js/2.958a21a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coffeeshop/static/js/2.958a21a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805e250d57609ae2cffc",
+    "url": "/coffeeshop/static/js/main.8d0e8212.chunk.js"
+  },
+  {
+    "revision": "70d6332c5a20063ad68b",
+    "url": "/coffeeshop/static/js/runtime-main.b61767f4.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/coffeeshop/static/media/getFetch.013f0b90.cjs"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc7bfb20efd5d41df9049e8dcc55fe22.js"
+  "/coffeeshop/precache-manifest.2b8ddbfa2ca9b37d5f2b5fcd889d2e42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coffeeshop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
